5b0bd01d85daaa054f17b2fa22388c911ca3029e
Koenig-Heinrich-der-4te <moritz_nickel@icloud.com>
2024-11-21 22:44:50 +0100

    extracted method

--- src/main/java/tronka/ordinarydiscordintegration/CommandHandler.java
+++ src/main/java/tronka/ordinarydiscordintegration/CommandHandler.java
@@ -6,6 +6,7 @@ import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.interactions.commands.OptionMapping;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.commands.build.SubcommandData;
+import tronka.ordinarydiscordintegration.linking.LinkManager;
 import tronka.ordinarydiscordintegration.linking.PlayerData;
 
 
@@ -31,57 +32,62 @@ public class CommandHandler extends ListenerAdapter {
     @Override
     public void onSlashCommandInteraction(SlashCommandInteractionEvent event) {
         if (event.getMember() == null || event.getMember().getGuild() != integration.getGuild()) { return; }
-        var linkManager = integration.getLinkManager();
         switch (event.getName()) {
             case "link" -> {
                 var code = event.getOptions().getFirst().getAsString();
-                var message = linkManager.confirmLink(event.getUser().getIdLong(), code);
+                var message = integration.getLinkManager().confirmLink(event.getUser().getIdLong(), code);
                 event.reply(message).setEphemeral(true).queue();
             }
             case "list" -> {
                 event.reply(String.join(", ", integration.getServer().getPlayerNames())).setEphemeral(true).queue();
             }
             case "linking" -> {
-                var user = event.getOption("user", OptionMapping::getAsUser);
-                if (user == null) {
-                    user = event.getUser();
-                }
-                var plink = linkManager.getDataOf(user.getIdLong());
-                switch (event.getSubcommandName()) {
-                    case "get" -> {
-                        String message;
-                        if (plink.isPresent()) {
-                            var link = plink.get();
-                            message = user.getAsMention() + " is linked to " + link.getPlayerName();
-                            var alts = link.getAlts();
-                            if (!alts.isEmpty()) {
-                                message += "\nWith " + alts.size() + " alts: " + String.join(", ", alts.stream().map(PlayerData::name).toList());
-                            }
-
-                        } else {
-                            message = user.getAsMention() + " is not linked to any player";
-                        }
-                        event.reply(message).setEphemeral(true).queue();
-                    }
-                    case "unlink" -> {
-                        if (plink.isEmpty()) {
-                            var message = user.equals(event.getUser()) ?
-                                    "You are not linked to any player" :
-                                    user.getAsMention() + " is not linked to any player";
-                            event.reply(message).setEphemeral(true).queue();
-                            return;
-                        }
-                        if (!user.equals(event.getUser()) && !event.getMember().getPermissions().contains(Permission.ADMINISTRATOR)) {
-                            event.reply("You do not have permission to use this command!").setEphemeral(true).queue();
-                            return;
-                        }
-                        linkManager.unlinkPlayer(user.getIdLong());
-                        event.reply("Successfully unlinked " + user.getAsMention()).setEphemeral(true).queue();
-                    }
-                    case null -> {}
-                    default -> throw new IllegalStateException("Unexpected value: " + event.getSubcommandName());
-                }
+                runLinkingCommand(event);
             }
+        }
+    }
+
+    private void runLinkingCommand(SlashCommandInteractionEvent event) {
+        var linkManager = integration.getLinkManager();
+
+        var user = event.getOption("user", OptionMapping::getAsUser);
+        if (user == null) {
+            user = event.getUser();
+        }
+        var plink = linkManager.getDataOf(user.getIdLong());
+        switch (event.getSubcommandName()) {
+            case "get" -> {
+                String message;
+                if (plink.isPresent()) {
+                    var link = plink.get();
+                    message = user.getAsMention() + " is linked to " + link.getPlayerName();
+                    var alts = link.getAlts();
+                    if (!alts.isEmpty()) {
+                        message += "\nWith " + alts.size() + " alts: " + String.join(", ", alts.stream().map(PlayerData::name).toList());
+                    }
+
+                } else {
+                    message = user.getAsMention() + " is not linked to any player";
+                }
+                event.reply(message).setEphemeral(true).queue();
+            }
+            case "unlink" -> {
+                if (plink.isEmpty()) {
+                    var message = user.equals(event.getUser()) ?
+                            "You are not linked to any player" :
+                            user.getAsMention() + " is not linked to any player";
+                    event.reply(message).setEphemeral(true).queue();
+                    return;
+                }
+                if (!user.equals(event.getUser()) && !event.getMember().getPermissions().contains(Permission.ADMINISTRATOR)) {
+                    event.reply("You do not have permission to use this command!").setEphemeral(true).queue();
+                    return;
+                }
+                linkManager.unlinkPlayer(user.getIdLong());
+                event.reply("Successfully unlinked " + user.getAsMention()).setEphemeral(true).queue();
+            }
+            case null -> {}
+            default -> throw new IllegalStateException("Unexpected value: " + event.getSubcommandName());
         }
     }
 }
