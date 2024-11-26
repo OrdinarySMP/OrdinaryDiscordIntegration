dcd7b15d38713fa60f21d4ae3750292fe77f9b60
Koenig-Heinrich-der-4te <moritz_nickel@icloud.com>
2024-11-26 21:12:04 +0100

    fix advancement message appearing falsely

--- src/main/java/tronka/ordinarydiscordintegration/mixin/AdvancementMixin.java
+++ src/main/java/tronka/ordinarydiscordintegration/mixin/AdvancementMixin.java
@@ -1,6 +1,5 @@
 package tronka.ordinarydiscordintegration.mixin;
 
-import net.minecraft.advancement.Advancement;
 import net.minecraft.advancement.AdvancementEntry;
 import net.minecraft.advancement.PlayerAdvancementTracker;
 import net.minecraft.server.network.ServerPlayerEntity;
@@ -8,7 +7,6 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import tronka.ordinarydiscordintegration.OrdinaryDiscordIntegration;
 
@@ -16,9 +14,10 @@ import tronka.ordinarydiscordintegration.OrdinaryDiscordIntegration;
 public class AdvancementMixin {
     @Shadow private ServerPlayerEntity owner;
 
-    @Inject(method = "grantCriterion", at = @At("HEAD"))
+    @Inject(method = "grantCriterion", at = @At(value = "INVOKE", target = "Lnet/minecraft/advancement/PlayerAdvancementTracker;onStatusUpdate(Lnet/minecraft/advancement/AdvancementEntry;)V"))
     private void receiveAdvancement(AdvancementEntry advancementEntry, String criterionName, CallbackInfoReturnable<Boolean> cir) {
         var advancement = advancementEntry.value();
+
         if (advancement != null && advancement.display().isPresent() && advancement.display().get().shouldAnnounceToChat()) {
             OrdinaryDiscordIntegration.getInstance().getChatBridge().onReceiveAdvancement(owner, advancement.display().get());
         }
