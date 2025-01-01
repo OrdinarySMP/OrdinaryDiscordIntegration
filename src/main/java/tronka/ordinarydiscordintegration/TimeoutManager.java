48c7ba452ef6b5858b2f53e8eed403d547086564
GoldenKoopa <100142836+GoldenKoopa@users.noreply.github.com>
2025-01-02 00:00:36 +0100

    add kick on leave

--- src/main/java/tronka/ordinarydiscordintegration/TimeoutManager.java
+++ src/main/java/tronka/ordinarydiscordintegration/TimeoutManager.java
@@ -3,13 +3,7 @@ package tronka.ordinarydiscordintegration;
 import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.events.guild.member.update.GuildMemberUpdateTimeOutEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
-import net.minecraft.server.MinecraftServer;
-import net.minecraft.server.network.ServerPlayerEntity;
-import net.minecraft.text.Text;
-import tronka.ordinarydiscordintegration.linking.PlayerData;
-import tronka.ordinarydiscordintegration.linking.PlayerLink;
 
-import java.util.Optional;
 
 public class TimeoutManager extends ListenerAdapter {
 
@@ -22,22 +16,7 @@ public class TimeoutManager extends ListenerAdapter {
     @Override
     public void onGuildMemberUpdateTimeOut(GuildMemberUpdateTimeOutEvent event) {
         Member member = event.getMember();
-        Optional<PlayerLink> playerLink = integration.getLinkManager().getDataOf(member.getIdLong());
-        if (playerLink.isEmpty()) {
-            return;
-        }
-        MinecraftServer server = integration.getServer();
-        ServerPlayerEntity player = server.getPlayerManager().getPlayer(playerLink.get().getPlayerId());
-        if (player != null) {
-            player.networkHandler.disconnect(Text.of(integration.getConfig().kickMessages.kickOnTimeOut));
-        }
-
-        for (PlayerData alt : playerLink.get().getAlts()) {
-            ServerPlayerEntity altPlayer = server.getPlayerManager().getPlayer(alt.getId());
-            if (altPlayer != null) {
-                altPlayer.networkHandler.disconnect(Text.of(integration.getConfig().kickMessages.kickOnTimeOut));
-            }
-        }
+        integration.getLinkManager().kickAccounts(member, integration.getConfig().kickMessages.kickOnTimeOut);
     }
 }
 
