f66d324334946cf5be30e4b61f40ad9216b4294a
GoldenKoopa <100142836+GoldenKoopa@users.noreply.github.com>
2025-01-02 13:59:32 +0100

    Add replies (#12)
    * add attachment support
    
    * add replies

--- src/main/java/tronka/ordinarydiscordintegration/config/Config.java
+++ src/main/java/tronka/ordinarydiscordintegration/config/Config.java
@@ -55,6 +55,7 @@ public class Config {
     public static class MessageStrings {
         public int chatMessageAttachmentColor = 0xff00ff;
         public String chatMessageFormat = "[§9Discord§r] <%user%> %msg%";
+        public String chatMessageFormatReply = "[§9Discord§r] <%user% replied to %userRepliedTo%> %msg%";
         public String commandExecutedInfoText = "%user% executed ``%msg%``";
         public String playerJoinMessage = "%user% joined";
         public String playerLeaveMessage = "%user% left";
