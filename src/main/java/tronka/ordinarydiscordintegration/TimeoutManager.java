39a26fdd9139177d19cc53716723dc430d966bee
GoldenKoopa <100142836+GoldenKoopa@users.noreply.github.com>
2025-01-02 13:54:17 +0100

    Kick on unlink dc (#10)
    * kick on timeout
    
    * kick on discord unlink

--- src/main/java/tronka/ordinarydiscordintegration/TimeoutManager.java
+++ src/main/java/tronka/ordinarydiscordintegration/TimeoutManager.java
@@ -3,7 +3,13 @@ package tronka.ordinarydiscordintegration;
 import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.events.guild.member.update.GuildMemberUpdateTimeOutEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.text.Text;
+import tronka.ordinarydiscordintegration.linking.PlayerData;
+import tronka.ordinarydiscordintegration.linking.PlayerLink;
 
+import java.util.Optional;
 
 public class TimeoutManager extends ListenerAdapter {
 
